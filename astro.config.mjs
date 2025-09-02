39db92f995f15bccfa5caff11fac6d3fb53c53e2
junbox98221 <junbox98221@gmail.com>
2025-09-02 20:42:24 +0900

    fix: Astro.site가 local에서 올바른 경로로 설정되도록 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,17 @@ import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { loadEnv } from 'vite';
-const { SITE_ORIGIN, VERCEL_BRANCH_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), '');
-// Determine site URL based on environment
+const { SITE_ORIGIN } = loadEnv(process.env.NODE_ENV, process.cwd(), '');
+
 const getSiteURL = () => {
-  // For Vercel production deployment
-  if (SITE_ORIGIN) {
-    return `https://${SITE_ORIGIN}`;
+  switch (process.env.NODE_ENV) {
+    case 'development':
+      return `http//${SITE_ORIGIN}`;
+    default:
+      return `https://${SITE_ORIGIN}`;
   }
-  // For Vercel preview deployment
-  if (VERCEL_BRANCH_URL) {
-    return loadEnv(`https://${VERCEL_BRANCH_URL}`);
-  }
-  // For local development
-  return 'http://localhost:4321';
 };
 
-// https://astro.build/config
 export default defineConfig({
   site: getSiteURL(),
   integrations: [tailwind(), react(), mdx(), sitemap()],
