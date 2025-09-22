0a0019e1d4615b6ec8b67b972ba6134b1b97a131
junbox98221 <junbox98221@gmail.com>
2025-09-22 14:29:32 +0900

    docs: firebase auth-2 작성

--- src/utils/heroImages.ts
+++ src/utils/heroImages.ts
@@ -6,6 +6,7 @@ import firebaseAuthHero from '../content/blog/Firebase-auth-1/asset/hero.png';
 import nextImageHero from '../content/blog/Next-image/asset/hero.png';
 import seoHero from '../content/blog/블로그 seo 개선/asset/hero.jpg';
 import performanceHero from '../content/blog/블로그 performance 개선/asset/hero.png';
+import firebaseAuth2Hero from '../content/blog/Firebase-auth-2/asset/hero.png';
 
 export const heroImages = {
   '가상-스크롤링': scrollHero,
@@ -13,6 +14,7 @@ export const heroImages = {
   'next-image': nextImageHero,
   '블로그-seo-개선': seoHero,
   '블로그-performance-개선': performanceHero,
+  'firebase-auth-2': firebaseAuth2Hero,
 } as const;
 
 export type HeroImageSlug = keyof typeof heroImages;
